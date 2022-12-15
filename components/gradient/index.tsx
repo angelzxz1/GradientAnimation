a647b68eb08eb7b88cfd731eee1593d4d143ea43
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2022-12-15 21:54:41 +0000

    added gradient functionality

--- components/gradient/index.tsx
+++ components/gradient/index.tsx
@@ -1,5 +1,44 @@
+import { Box, keyframes } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
+const spin = keyframes`
+  from {transform: rotate(0deg);}
+  to {transform: rotate(-360deg)}
+`;
+
 const Gradient = () => {
-  return <div>gradient</div>;
+  const [colorHUEl, setColorHUEl] = useState(Math.floor(Math.random() * 180));
+  const [colorHUEr, setColorHUEr] = useState(
+    Math.floor(Math.random() * 180) + 180
+  );
+  const spinAnimation = `${spin} infinite 2s linear`;
+  useEffect(() => {
+    setInterval(() => {
+      setColorHUEl((prev: number) => {
+        // console.log(prev);
+        if (prev === 360) {
+          return 0;
+        }
+        return prev + 1;
+      });
+      setColorHUEr((prev: number) => {
+        // console.log(prev);
+        if (prev === 360) {
+          return 0;
+        }
+        return prev + 1;
+      });
+    }, 150);
+  }, []);
+  return (
+    <Box
+      w="200px"
+      h="200px"
+      borderRadius="full"
+      animation={spinAnimation}
+      bgGradient={`linear(to-r, hsl(${colorHUEl},75%,25%), hsl(${colorHUEr},75%,25%))`}
+    />
+  );
 };
 
 export default Gradient;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <Box>
       <Gradient />
+      <Gradient />
     </Box>
   );
 };
